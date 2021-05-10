b307332c82a7abc75fd296c151a4cce4017ad738
Dawid Iwaniuk <dawuso@gmail.com>
2021-05-10 21:27:42 +0200

    Dokończenie

--- FizzBuzz/Startup.cs
+++ FizzBuzz/Startup.cs
@@ -9,7 +9,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FizzBuzz.Data;
-using FizzBuzz.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 //using DependencyInjectionSample.Interfaces;
--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -5,7 +5,8 @@ CREATE PROCEDURE [dbo].[FBAdd]
 		@Number int,
 		@Wynik VARCHAR (50),
 		@Data DATETIME,
-		@Id int OUTPUT
+		@Id int OUTPUT,
+		@OwnerID VARCHAR (50)
 AS 
-		INSERT INTO DoBazy (Number, Wynik, Data) Values (@Number, @Wynik, @Data)
+		INSERT INTO DoBazy (Number, Wynik, Data, OwnerID) Values (@Number, @Wynik, @Data, @OwnerID)
 		SET @Id = @@IDENTITY
